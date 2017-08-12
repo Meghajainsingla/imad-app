8a32f877d64dde15866b21b87d232562cf1dbdc1
Meghajainsingla <meghajainsingla@gmail.com>
2017-08-12 13:54:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the text of main-text
 var element= document.getElementById('main-text');
 element.innerHTML='New-value';
+
+//move the img
+var img=document.getElementById('madi');
+img.onclick= function()
+{
+    img.style.marginLeft= '100px';
+};
